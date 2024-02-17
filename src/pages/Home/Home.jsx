c8bc7bf6b5f43c1f161d6e908aab57ac6c9b362c
unknown <mdzuneadabedineidmum@gmail.com>
2024-02-18 05:55:59 +0600

    aa finish

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import Card from '../Card/Card';
+import React, { useEffect, useState } from "react";
+import Card from "../Card/Card";
 
 const Home = () => {
-    const [products,setProducts]=useState([]);
-    
-    useEffect(()=>{
-        const disconnect=()=>fetch('https://panda-commerce-server.onrender.com/products').then(res=>res.json()).then(data=>setProducts(data))
-        return ()=>disconnect();
-    },[])
-    return (
-        <div style={{margin:"0 auto"}} className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-7 mx-auto'>
-           {
-            products.map(product=><Card products={products} setProducts={setProducts} product={product} key={product._id}></Card>)
-           }
-        </div>
-    );
+  const [products, setProducts] = useState([]);
+  console.log(products);
+  useEffect(() => {
+    fetch("https://panda-commerce-server.onrender.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  return (
+    <div
+      style={{ margin: "0 auto" }}
+      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-7 mx-auto"
+    >
+      {products.map((product) => (
+        <Card
+          products={products}
+          setProducts={setProducts}
+          product={product}
+          key={product._id}
+        ></Card>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
